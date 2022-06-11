f10239d5510f3d75dddcceda8421396375a8d4c6
ipmdevops <38317646+ipmdevops@users.noreply.github.com>
2022-06-11 17:21:16 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "this is first time you gusy are doing pipeline so wait !!";
+        return "this is second time you gusy are doing pipeline so wait !!";
     }
 
 }
