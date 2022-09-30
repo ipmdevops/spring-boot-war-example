4e3fa35d65cebc5a124e248ddce7aaa82b7207be
srevivek <112747188+srevivek@users.noreply.github.com>
2022-09-30 23:25:07 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "It was fantastic journey with Jenkins !!";
+        return "It was fantastic journey with Jenkins  second!!";
     }
 
 }
