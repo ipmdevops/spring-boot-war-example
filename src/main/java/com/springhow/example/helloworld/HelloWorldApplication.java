d032d1887236b9237d7e35d4bb2ba92009f2bbf1
ipmdevops <38317646+ipmdevops@users.noreply.github.com>
2023-04-30 10:43:02 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "It is always good to learn something new Saurabh...!!";
+        return "It is always good to learn something new Saurabh... Dhanjay must start learning Linux to understand to VI commands!!";
     }
 
 }
