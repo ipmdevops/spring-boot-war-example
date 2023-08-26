52050221ab812ee13dd16b9014244b29f68fa92e
ipmdevops <38317646+ipmdevops@users.noreply.github.com>
2023-08-26 11:01:08 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "This devops batch is learning Jenkins deployment and Git now and learning good!!";
+        return "This devops batch is learning Jenkins deployment and Git now and learning good!! Also added new function webhook";
     }
 
 }
